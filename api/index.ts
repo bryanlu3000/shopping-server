2a723a13fac4a50594f390bad18f825c46c3b90a
Bryan <bryan2017canada@gmail.com>
2022-11-06 14:33:00 -0500

    udpated for vercel

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
-const express = require("express");
-// import express, { Request, Response } from "express";
+// const express = require("express");
+import express from "express";
 
 const app = express();
 

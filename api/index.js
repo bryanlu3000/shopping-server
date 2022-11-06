836760102d241187dd117f72e4fad6a00b64d5a7
Bryan <bryan2017canada@gmail.com>
2022-11-06 12:06:23 -0500

    udpated for vercel

--- api/index.js
+++ api/index.js
@@ -1,9 +1,9 @@
-// const express = require("express");
-import express, { Request, Response } from "express";
+const express = require("express");
+// import express, { Request, Response } from "express";
 
 const app = express();
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/", (req, res) => {
   res.send("Express on Vercel");
 });
 
